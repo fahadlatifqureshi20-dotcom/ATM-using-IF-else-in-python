edd591b70b6498fcc6f7a315afd73cf77c0c7b21
M Fahad latif <fahadlatifqureshi20@gmail.com>
2026-07-17 18:21:41 +0500

    Refactor ATM project using functions
    Refactored the ATM project by organizing the code into reusable functions. Added separate functions for card validation, PIN validation, menu handling, balance checking, and cash withdrawal. Also implemented parameter passing and return values to manage the account balance more efficiently while keeping the program logic cleaner and easier to maintain.

--- ATM.py
+++ ATM.py
@@ -1,54 +1,102 @@
 balance = 5000 
-ammount = 0
-attempts=0
-card = input("Please insert your card: ")
-if card == "valid_card":
-    while attempts < 3: 
-      pin = input("Please enter your PIN: ")
+amount = 0
+opt = 0
+
+def card_check():
+  '''
+  checks if the card is valid or not
+  '''
+  
+  card = input("Please insert your card: ")
+  if card == "valid_card":
+   return  True
+  
+  else :
+    exit("Invalid card. Please try again.")
+
+
+
+def pin_check ():
+    '''
+     checks if the pin is valid or not
+     '''
+    attempts=0
+    while attempts < 3:
+      pin= input("Please enter your PIN: ")
       if pin == "1234":
-        break
+        print("Access granted. Welcome!")
+      
+        return True
+        
       else:
         attempts += 1
         print(f"Invalid PIN. {3 - attempts} attempts left.")
         if attempts == 3:
             print("Too many incorrect attempts. Access denied.")
             exit()
-    
-    print("Access granted. Welcome!")
+            
+def menu():
+    print("Please select an option:")
+    print("1. Balance check")
+    print("2. Withdraw cash")
+    print("3. Exit")
+    opt = input()
+    return opt
                 
-    while True:    
-               
-        print("choose option => ","balance check ","withdraw cash ", "exit ") 
-    
-        option = input()
-       
 
-        if option == "exit":
-            print("Thank you for using our ATM. Goodbye!")
-            exit()
+def exit_menu ():
+  print("Thank you for using our ATM. Goodbye!")
+     
 
-        elif option == "balance check":
-          print(f"Your current balance is = ${balance}.")
+def balance_check(show_balance):
+  
+      print(f"Your current balance is = ${show_balance}.")
           
+def withdraw(local_balance):
+        # if opt == "withdraw cash":
+           
+          while True:
+           print("enter the amount you want to withdraw => .")
+         
+           amount = int(input())
+           if amount<1000 or amount>local_balance or amount%500 != 0:
+            print(" dicliend! invalid ammount . press 1 to re-enter the ammount or 0 to exit")
+            con=input()
+            if con == "1":
+              continue
+            else:
+              exit()  
+           else:
+            print(f"transection completed! please collect your cash= $ {amount}")
+            local_balance -= amount
+            print(f"Your remining balance is ${local_balance}. press 1 to continue or 0 to exit")
+            if input() == "1":
+              continue
+            else:   
+              return local_balance
+  
+ 
+chk_card=card_check()
+chk_pin=pin_check
 
-        elif option == "withdraw cash":
-         print("enter the amount you want to withdraw => .")
+if chk_card  :
 
-         ammount = int(input())
-         if ammount<1000 or ammount>balance or ammount%500 != 0:
-            print(" dicliend! invalid ammount")
-            exit() 
-         else:
-            print(f"transection completed! please collect your cash= $ {ammount}")
-            balance=balance-ammount
-            print(f"Your remining balance is ${balance}.") 
-             
-        else:  print("invalid option! please choose a valid option")   
-        print('press 1 to continue or 0 to exit')    
-        choice = input()
-        if choice == "0":
-            print("Thank you for using our ATM. Goodbye!")
-            exit()
-else:       
-    print("Invalid card. Access denied.")
+    if chk_pin():
+      while True:
+       option=menu()
+      
+       if option =="balance check":
+        balance_check(balance)
+       elif option=="withdraw cash":
+        balance_update= withdraw(balance)
+        balance=balance_update
+       elif option=="exit":
+        exit_menu()
+        break
+       else:
+        print("Invalid option. Please try again.")
 
+        
+       
+        
+       
